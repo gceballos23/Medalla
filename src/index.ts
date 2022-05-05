8c7dcfd8dbb0479708bbee35311c0d45a0c23c40
gceballos23 <103612856+gceballos23@users.noreply.github.com>
2022-05-05 20:41:52 -0300

    switch ejercicio hecho con switch

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
-let data = document.getElementById("data");
 let btnSend = document.getElementById("send");
 
-btnSend.addEventListener("click",() => {
-  switch(Number(data.value)){
+btnSend.addEventListener("click", () => {
+  let posicionLLegada: number = Number(data.value);
+  switch (posicionLLegada) {
     case 1:
-        console.log("Entregar medalla de oro");
-        break;
+      console.log("Entregar medalla de oro");
+      break;
     case 2:
-        console.log("Entregar medalla de plata");
-        break;
+      console.log("Entregar medalla de plata");
+      break;
 
     case 3:
-        console.log("Entregar Medalla de Bronce");
-        break;
+      console.log("Entregar Medalla de Bronce");
+      break;
     default:
-        console.log("NO recibe medalla");
-
+      console.log("NO recibe medalla");
   }
-
 });
